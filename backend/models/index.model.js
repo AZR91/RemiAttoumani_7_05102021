77e755b1bcb8948d30908c5625e7667d7a0fb04a
azr91 <remiattoumani@gmail.com>
2021-10-08 16:25:03 +0200

    création fichier index.model.js

---
+++ backend/models/index.model.js
@@ -0,0 +1,94 @@
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+const dotenv = require('dotenv').config()
+const db = {}
+
+// @ts-ignore
+const sequelize = new Sequelize('mysql://root:root@localhost:3306/')
+
+db.Gif = require('./Gif.model')(sequelize, Sequelize);
+db.User = require('./User.model')(sequelize, Sequelize);
+db.Comment = require('./Comment.model')(sequelize, Sequelize);
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+db.User.belongsToMany(db.Gif,
+{
+    through: "Like_Gif",
+    foreignKey: 'userId',
+    onDelete: 'CASCADE',
+    onUpdate: 'NO ACTION',
+    allowNull: false
+}
+);
+db.User.belongsToMany(db.Gif,
+{
+    through: "Dislike_Gif",
+    foreignKey: 'userId',
+    onDelete: 'CASCADE',
+    onUpdate: 'NO ACTION',
+    allowNull: false
+}
+);
+db.Gif.belongsTo(db.User, 
+{
+    foreignKey: {
+    name: 'userId',
+    allowNull: false
+    },
+    onDelete: 'CASCADE',
+    onUpdate: 'NO ACTION',
+}
+);
+
+db.Gif.belongsToMany(db.User,
+{
+    through: "Like_Gif",
+    foreignKey: 'gifId',
+    onDelete: 'CASCADE',
+    onUpdate: 'NO ACTION',
+    allowNull: false
+}
+);
+db.Gif.belongsToMany(db.User,
+{
+    through: "Dislike_Gif",
+    foreignKey: 'gifId',
+    onDelete: 'CASCADE',
+    onUpdate: 'NO ACTION',
+    allowNull: false
+}
+);
+db.Comment.belongsTo(db.User,
+{
+    foreignKey: {
+    name: 'userId',
+    allowNull: false
+    },
+    onDelete: 'CASCADE',
+    onUpdate: 'NO ACTION',
+}
+);
+db.Comment.belongsTo(db.Gif,
+{
+    foreignKey: {
+    name: 'gifId',
+    allowNull: false,
+},
+    onDelete: 'CASCADE',
+    onUpdate: 'NO ACTION',
+}
+);
+
+try {
+sequelize.authenticate();
+console.log('Connection has been established successfully.');
+} catch (error) {
+console.error('Unable to connect to the database:', error);
+}
+
+sequelize.sync({});
+
+module.exports = db;
